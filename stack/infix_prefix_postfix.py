1c4245fa7f473605dff7d62d118038640c7fd997
shubham.pandey <shubham.pandey@ihsmarkit.com>
2021-03-13 19:29:50 +0530

    added more scenarios

---
+++ stack/infix_prefix_postfix.py
@@ -0,0 +1,11 @@
+from stack_ds import Stack
+
+def infix_to_postfix(infix_expression):
+    pass
+
+
+if __name__ == "__main__":
+    infix_expression = "a+b*c+d"
+    print(f"Infix expression {infix_expression}")
+    print(f"calling infix to postfix method")
+    
---
+++ stack/stack_ds.py
@@ -0,0 +1,28 @@
+class Stack(object):
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+        print(f"pushed {value} to stack")
+
+    def pop(self):
+        popped_elem = self.stack.pop()
+        print(f"popped out {popped_elem}")
+
+    def __str__(self):
+        return str(self.stack)
+
+if __name__ == "__main__":
+    stack = Stack()
+    stack.push(10)
+    stack.push(150)
+    stack.push(87)
+    stack.push(12)
+    stack.push(90)
+    print (stack)
+    stack.pop()
+    print (stack)
+
+
+
